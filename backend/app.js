1234926f3fb8deddfc756f8a40f092e2c1afdbe1
Nathalie Lindqvist <nathalielindqvist@gmail.com>
2021-11-04 16:50:09 +0100

    Backend server

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+"use strict";
+
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const config = require('./db');
+
+const users = require('./routes/user');
+
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+    () => {console.log('Database is connected') },
+    err => { console.log('Can not connect to the database'+ err)}
+);
+
+const app = express();
+app.use(passport.initialize());
+require('./passport')(passport);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/api/users', users);
+
+app.get('/', function(req, res) {
+    res.send('hello');
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
